bd3c379ceb1a8345232e1e924e401ac017663f7c
Wesley Knight <31743477+dilutedspine@users.noreply.github.com>
2018-05-16 11:14:23 -0500

    Update laurelYanny.ps1

--- laurelYanny.ps1
+++ laurelYanny.ps1
@@ -1,10 +1,15 @@
-Add-Type -AssemblyName System.speech
-$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$count = 1
-write-host "Press ctrl+c to cancel"
-while($count -ne 0)
+get-laurelyanny
 {
-	$speak.Speak('Laurel')
-	Start-Sleep -s .5
-    $speak.Speak('Yanny')
+    Add-Type -AssemblyName System.speech
+    $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $count = 1
+    write-host "Press ctrl+c to cancel"
+    while($count -ne 0)
+    {
+        Write-Host "Laurel"
+    	$speak.Speak('Laurel')
+    	Start-Sleep -s .5
+        Write-Host "Yanny"
+        $speak.Speak('Yanny')   
+    }
 }
